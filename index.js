f9565e264edb6e222adf436c75f27060aaf6e778
Tom Zhang <14881754+FlappiTomic@users.noreply.github.com>
2021-07-07 15:01:23 -0400

    Change to commonjs syntax

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import utils from './lib/utils.js'
-import InvalidArgumentException from './lib/invalid-argument-exception.js'
-export { utils }
-export { InvalidArgumentException }
+const utils = require('./lib/utils.js')
+const InvalidArgumentException = require('./lib/invalid-argument-exception.js')
+module.exports.utils = utils
+module.exports.InvalidArgumentException = InvalidArgumentException
